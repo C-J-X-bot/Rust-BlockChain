d814cf8600cd1ada9381d223837d1d05647b066a
cjx <cjx113949@163.com>
2026-04-29 00:21:33 +0800

    config_set update

--- pxsol-ss-main/pxsol-ss-client/src/main.rs
+++ pxsol-ss-main/pxsol-ss-client/src/main.rs
@@ -4,7 +4,6 @@ use serde::{Deserialize, Serialize};
 use std::{
     fs::{self, File},
     io::{BufReader, Write},
-    // path::Path,
 };
 
 const CONFIG_FILE_PATH: &str = ".pxsol-ss-client/config.json";
@@ -17,7 +16,14 @@ fn main() -> Result<()> {
             program_id,
             network,
             get,
-        } => config_set(program_id, network)?,
+        } => {
+            if get {
+                let _ = get_current_config();
+            }
+            if !program_id.is_none() || !network.is_none() {
+                let _ = config_set(program_id, network)?;
+            }
+        }
     }
     Ok(())
 }
@@ -34,7 +40,7 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Config {
-        #[arg(short = 'p', long = "programid")]
+        #[arg(short = 'p', long = "programid",)]
         program_id: Option<String>,
 
         #[arg(short = 'n', long = "network")]
@@ -45,15 +51,32 @@ enum Commands {
     },
 }
 
-pub fn config_set(program_id: Option<String>, network: Option<String>) -> Result<()> {
-    // 文件路径拼接
+fn get_config_file_path() -> std::path::PathBuf {
     let home_path = dirs::home_dir().expect("无法获取 home 目录");
-    let path = home_path.join(CONFIG_FILE_PATH);
+    return home_path.join(CONFIG_FILE_PATH);
+}
 
-    println!(
-        "新传入配置为 program_id:{:?} , network: {:?}",
-        program_id, network
-    );
+fn get_current_config() -> Result<()> {
+    let path = get_config_file_path();
+    if !path.exists() {
+        println!("配置文件不存在!")
+    } else {
+        let file = File::open(path.clone())?;
+        let reader = BufReader::new(file);
+        let config_json: ConfigJson = serde_json::from_reader(reader)?;
+        println!(
+            "当前配置 program_id:{} , network: {} ",
+            config_json.program_id, config_json.network
+        );
+    }
+    Ok(())
+}
+
+fn config_set(program_id: Option<String>, network: Option<String>) -> Result<()> {
+    let path = get_config_file_path();
+
+    // 设置默认配置
+    let mut config_json: ConfigJson = ConfigJson::new();
 
     if !path.exists() {
         //文件不存在创建文件
@@ -63,24 +86,14 @@ pub fn config_set(program_id: Option<String>, network: Option<String>) -> Result
         if let Some(parent) = path.parent() {
             let _ = fs::create_dir_all(parent);
         }
-        if path.exists() {
-            println!("创建成功");
-        } else {
-            println!("创建失败");
-        }
     } else {
-        //文件已存在
-        let file = File::open(path.clone())?;
+        // 文件存在读取文件内容
+        println!("配置文件已存在");
+        let file = File::open(&path)?;
         let reader = BufReader::new(file);
-        let config_json: ConfigJson = serde_json::from_reader(reader)?;
-        println!(
-            "配置文件已存在 当前配置为 program_id:{} , network: {} ",
-            config_json.program_id, config_json.network
-        );
+        config_json = serde_json::from_reader(reader)?;
     }
 
-    // 配置写入
-    let mut config_json = ConfigJson::new();
     if !program_id.is_none() {
         config_json.program_id = program_id.clone().unwrap();
     }
@@ -91,6 +104,7 @@ pub fn config_set(program_id: Option<String>, network: Option<String>) -> Result
     let mut file = File::create(&path)?;
     file.write_all(json_string.as_bytes())?;
 
+    let _ = get_current_config();
     Ok(())
 }
 
@@ -105,13 +119,6 @@ impl ConfigJson {
         ConfigJson {
             program_id: String::new(),
             network: String::new(),
-        }
-    }
-
-    fn get(self)->ConfigJson{
-        ConfigJson {
-            program_id: self.program_id,
-            network: self.network,
         }
     }
 }
